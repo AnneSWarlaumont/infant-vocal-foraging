dc4b58b29933f4b68f2cb9ddaf469e8c4c5270a2
Ritwikavps <38600536+Ritwikavps@users.noreply.github.com>
2019-02-09 22:25:37 -0800

    Update corrltn_range_advoc.m

--- Analyses/LENA_data_analysis/correlation/corrltn_range_advoc.m
+++ Analyses/LENA_data_analysis/correlation/corrltn_range_advoc.m
@@ -94,7 +94,7 @@ for i = 1:length(id_age)
     corrltn_ad(i_count,1) = r(1,2);
     corrpval_ad(i_count,1) = p(1,2);
     
-    samplsi(i_count,1) = length(sp);
+    samplsi(i_count,1) = length(sp);%note that samplesize is after removing NaN entries
     agestr = strsplit(id_age{i},'_');  %finds age by splitting the id_age string
     id{i_count,1} = agestr{1};
     age(i_count,1) = str2num(agestr{2});
