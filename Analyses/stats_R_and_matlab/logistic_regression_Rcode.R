32f542d7dd7dcaa64b7eb2c9b08388aabd961c1d
AnneSWarlaumont <anne.warlaumont@gmail.com>
2019-08-20 13:45:20 -0700

    Made logistic_regression_Rcode.R get input files from OSF.

--- Analyses/stats_R_and_matlab/logistic_regression_Rcode.R
+++ Analyses/stats_R_and_matlab/logistic_regression_Rcode.R
@@ -5,14 +5,27 @@
 
 # Set the working directory to the directory containing the csv file
 # Modify the line below for your own file system
-setwd("/Users/ritu/Google Drive/research/vocalisation/clean_code_thats_used/csv_files_vfinal")
+# setwd("/Users/ritu/Google Drive/research/vocalisation/clean_code_thats_used/csv_files_vfinal")
+# desiredfiles = dir(path=".", pattern="*logisticreg") # Read all files that contain "logisticreg"
+
+# Or, get the files from OSF (https://osf.io/8ern6/files/
+# You will need the osfr package, https://centerforopenscience.github.io/osfr/)
+# To get set up, run the following three lines of code:
+# install.packages("remotes")
+# library(remotes)
+# remotes::install_github("centerforopenscience/osfr")
+library(osfr)
+# If needed, modify the line below for your own file system
+setwd("~/Downloads")
+osf_retrieve_file("https://osf.io/vz9qy/") %>% osf_download() # Downloads advoc_logisticreg_stepsizes.csv
+osf_retrieve_file("https://osf.io/sp2d7/") %>% osf_download() # Downloads advoc_logisticreg.csv
+osf_retrieve_file("https://osf.io/wamzy/") %>% osf_download() # Downloads chvoc_logisticreg_stepsizes.csv
+osf_retrieve_file("https://osf.io/8bn4t/") %>% osf_download() # Downloads chvoc_logisticreg.csv
+desiredfiles = c("advoc_logisticreg_stepsizes.csv","advoc_logisticreg.csv","chvoc_logisticreg_stepsizes.csv","chvoc_logisticreg.csv")
 
 library(lme4)
 library(lmerTest)
 library(pracma)
-
-# Read all file sin folder
-desiredfiles = dir(path=".", pattern="*logisticreg")
 
 vocaliser <- c() #assign vectors to be filled in
 measure <- c()
@@ -104,7 +117,7 @@ if ((dim(ss)[1] == 3)&(strcmp(ss[3],"stepsizes.csv") == TRUE)) { #for all lena s
 
 }
 
-setwd("/Users/ritu/Google Drive/research/vocalisation/clean_code_thats_used/csv_files_vfinal/Reported_results") #cd into folder to save into
+# setwd("/Users/ritu/Google Drive/research/vocalisation/clean_code_thats_used/csv_files_vfinal/Reported_results") #cd into folder to save into
 
 logistic_regression_results <- cbind(vocaliser,measure,age_effect,age_pval,freq_effect,freq_pval,amp_effect,amp_pval,freqst_effect,freqst_pval,ampst_effect,ampst_pval,timst_effect,timst_pval) #make dataframe
 
