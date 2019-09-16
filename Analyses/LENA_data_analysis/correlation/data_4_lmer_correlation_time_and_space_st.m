03aa9d8a5032af472f7532c3ac3260e905dc522a
Ritwikavps <38600536+Ritwikavps@users.noreply.github.com>
2019-09-16 00:03:56 -0700

    Add files via upload

--- Analyses/LENA_data_analysis/correlation/data_4_lmer_correlation_time_and_space_st.m
+++ Analyses/LENA_data_analysis/correlation/data_4_lmer_correlation_time_and_space_st.m
@@ -19,6 +19,16 @@ T = lmer_corrltn_data(adr_id,adr_zd,adr_zlogf,adr_age,adr_st,adr_en,adresponse);
 
 writetable(T,'chvoc_lmercorrltn.csv')
 
+%make table for WR only
+WR_in = T.response == 1;
+T_WR = T(WR_in,:);
+writetable(T_WR,'chvoc_lmercorrltn_WR.csv')
+
+%make table for WOR only
+WOR_in = T.response == 0;
+T_WOR = T(WOR_in,:);
+writetable(T_WOR,'chvoc_lmercorrltn_WOR.csv')
+
 clear all
 
 %-0----------------------------------------0----------------------------------------0----------------------------------------
@@ -30,6 +40,16 @@ load('chresp_zkm_match.mat')
 T = lmer_corrltn_data(chr2ad_id,chr2ad_zd,chr2ad_zlogf,chr2ad_age,chr2ad_st,chr2ad_en,chr2ad);
 
 writetable(T,'advoc_lmercorrltn.csv')
+
+%make table for WR only
+WR_in = T.response == 1;
+T_WR = T(WR_in,:);
+writetable(T_WR,'advoc_lmercorrltn_WR.csv')
+
+%make table for WOR only
+WOR_in = T.response == 0;
+T_WOR = T(WOR_in,:);
+writetable(T_WOR,'advoc_lmercorrltn_WOR.csv')
 
 clear all
 
